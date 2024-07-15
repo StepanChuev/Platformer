f10ba3ed4aef060a4e4f29a5dfab9d53573059c9
StepanChuev <133255497+StepanChuev@users.noreply.github.com>
2024-07-15 21:01:28 +0400

    Update walkingMine.js

--- platformer/walkingMine.js
+++ platformer/walkingMine.js
@@ -1,43 +1,41 @@
 'use strict';
 
-const defineDirection = (unit, otherParams) => () => {
-	return ((unit.x <= otherParams.leftX && otherParams.direction === "left") ? "right" : (unit.x >= otherParams.rightX && otherParams.direction === "right") ? "left": otherParams.direction);
-};
+function WMUpdateFunctions(unit, otherParams){
+	const funcs = {
+		defineDirection(){
+			return ((unit.x <= otherParams.leftX && otherParams.direction === "left") ? "right" : (unit.x >= otherParams.rightX && otherParams.direction === "right") ? "left": otherParams.direction);
+		},
 
-const walke = (unit, otherParams) => () => {
-	otherParams.direction = defineDirection(unit, otherParams)();
-	unit.updateFunctions.run((otherParams.direction === "left") ? 37 : 39);
-};
+		walke(){
+			otherParams.direction = this.defineDirection();
+			unit.updateFunctions.run((otherParams.direction === "left") ? 37 : 39);
+		},
 
-const isCollideWithEnemies = (unit, otherParams) => () => {
-	for (let i = 0; i < unit.enemies.length; i++){
-		if (unit.updateFunctions.isCollideWithEnemy(unit.enemies[i])){
-			return true;
-		}
-	}
-
-	return false;
-};
-
-const collision = (unit, otherParams) => () => {
-	if (isCollideWithEnemies(unit, otherParams)()){
-		for (let i = 0; i < unit.enemies.length; i++){
-			if (distance(unit.x, unit.y, unit.enemies[i].x, unit.enemies[i].y) < otherParams.damageRadius){
-				unit.enemies[i].health -= otherParams.damage;
-				unit.health = -1;
+		isCollideWithEnemies(){
+			for (let i = 0; i < unit.enemies.length; i++){
+				if (unit.updateFunctions.isCollideWithEnemy(unit.enemies[i])){
+					return true;
+				}
 			}
+
+			return false;
+		},
+
+		collision(){
+			if (this.isCollideWithEnemies()){
+				for (let i = 0; i < unit.enemies.length; i++){
+					if (distance(unit.x, unit.y, unit.enemies[i].x, unit.enemies[i].y) < otherParams.damageRadius){
+						unit.enemies[i].health -= otherParams.damage;
+						unit.health = -1;
+					}
+				}
+
+				return true;
+			}
+
+			return false;
 		}
-
-		return true;
-	}
-
-	return false;
-};
-
-
-const getWmUpdateFunctions = (unit, otherParams) => {
-	return {
-		defineDirection: defineDirection(unit, otherParams), walke: walke(unit, otherParams),
-		isCollideWithEnemies: isCollideWithEnemies(unit, otherParams), collision: collision(unit, otherParams) 
 	};
-};
+
+	return funcs;
+}
